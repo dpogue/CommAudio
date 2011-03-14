11386a528a26899f80c49f4e59e2ba02ea12cdc4
Kelvin <kelvinlui80@hotmail.com>
2011-03-13 20:00:39 -0700

    added connect/listen to commsocket

--- commsocket.h
+++ commsocket.h
@@ -8,16 +8,18 @@ class CommSocket : public QWidget
 {
 	Q_OBJECT
 private:
-	SOCKET createSocket(HWND hwnd,int mode,int port,int protocol);
+	SOCKET createSocket(HWND hwnd,QString host,int mode,int port,int protocol);
     SOCKET sock;
+	sockaddr_in server;
+
     
 public:
     CommSocket(QString host, int port,int protocol);
-       
+	bool connectToServ();
+	bool listenForConn();
     
 protected:
     virtual bool winEvent(MSG* message, long* result);
-
 
 signals:
     void connected();
