e1b98d9add21d8be9d9e521bc3858450e6e984cb
unknown <Terry@.(none)>
2011-03-14 13:58:57 -0700

    Cleaned up waitting on Qt to install to test

--- manager.h
+++ manager.h
@@ -12,56 +12,14 @@
 #include <vorbis/vorbisfile.h>
 #include <errno.h>
 #include <sys/types.h>
-#include "../util/mutex_magic.h"
 
 #define QUEUESIZE 8
 #define BUFFERSIZE (1024*32)
 
-enum SoundType {
-     sfx,
-     ntf,
-};
-
-/**
- * The client-side audio manager class.
- * A client must call the instance() method first to create and  
- * initialize the singleton instance. Once a call to instance() has been made,
- * this class is thread-safe.
- *
- * Internally in the class everytime playSfx is called, It creates
- * a thread specifically to play that file to completion.
- * To play music You can build a QQueue of filenames and pass that
- * to playMusic(). playMusic() loops through the queue, Meaning after 
- * finishing a song it puts it in the back of the queue. 
- *
- * The only way to stop a sound from playing is by calling shutdown().
- * After this is called if you wish to play sounds again you 
- * need to call startup();
- *
- * A typical use of the AudioManager class looks like this:
- * @code
- *   QString filename = "fire.ogg";
- *   QQueue musicList = td::AudioManager::instance()->musicDir("sound/music");
- *
- *   //This is the same call as playSfx(filename, sfx);
- *   td::AudioManager::instance()->playSfx(filename);
- *   //If the sound is a notification add ntf to the argument list
- *   td::AudioManager::instance()->playSfx(filename, ntf);
- *
- *   td::AudioManager::instance()->playMusic(musicList);
- *
- *   //call when you wish all sounds to stop
- *   td::AudioManager::instance()->shutdown();
- * @endcode
- *
- * @author Darryl Pogue
- * @author Terence Stenvold
- */
 
 class AudioManager : public QObject {
     Q_OBJECT
 
-    THREAD_SAFE_SINGLETON
 
 private:
     /**
@@ -73,30 +31,10 @@ private:
     static AudioManager* instance_;
 
     /**
-     * Array containing possible Gain levels
-     */
-    static float gainScale[];
-
-    /**
-     * The volume/gain of the sound effects.
-     */
-    int sfxGain_;
-
-    /**
-     * The volume/gain of the notifications.
-     */
-    int notiGain_;
-
-    /**
      * The volume/gain of the background music.
      */
     int musicGain_;
 
-    /**
-     * The number of audio tracks playing
-     */
-    int playing_;
- 
     /**
      * Whether the AudioManager has been initialized.
      *
@@ -128,16 +66,7 @@ private:
      * @param filename the path to file.
      * @param gain is a float with a default param of 1.0
      */
-    void streamOgg(QString filename, float gain = 1);
-
-    /**
-     * Goes through all the filenames in the queue
-     * it continously loops all filenames
-     *
-     * @author Terence Stenvold
-     * @param filenameQueue queue<QString> of filenames of ogg files.     *
-     */
-    void playMusicQueue(QQueue<QString> filenameQueue);
+    void streamOgg(QString filename);
 
 public:
 
@@ -180,51 +109,13 @@ public:
     void startup();
     
     /**
-     * Plays an Ogg Vorbis sound file.
+     * Play the Ogg Vorbis sound file as background music.
      *
      * @author Terence Stenvold
-     * @param filename The path to the .ogg file.
-     * @param type is a Enum either ntf for notifcation or sfx. defaults sfx. 
-     */
-    void playSfx(QString filename, SoundType type=sfx);
-
-    /**
-     * Overloaded function that randomly picks and 
-     * Plays an Ogg Vorbis sound file.
-     *
-     * @author Terence Stenvold
-     * @param files the vector of filesname paths to the .ogg files.
-     * @param type is a Enum either ntf for notifcation or sfx. defaults sfx. 
-     */
-    void playSfx(QVector<QString> files, SoundType type=sfx);
-
-    /**
-     * Play an Ogg Vorbis sound file, looping when it reaches the end.
-     * !!!NOT IMPLEMENTED!!!
-     *
-     * @author Terence Stenvold
-     * @param filename The path to the .ogg file.
-     * @param loop The number of times to loop the sound file. The default 
-     * of -1 results in infinite looping.
-     */
-    void loopSfx(QString filename, int loop = -1);
-
-    /**
-     * Play the Queue of Ogg Vorbis sound files as background music.
-     *
-     * @author Terence Stenvold
-     * @param filenameQueue queue<QString> of filenames of ogg files.
+     * @param filename QString of ogg file.
      */    
-    void playMusic(QQueue<QString> filenameQueue);
+    void playMusic(QString filename);
 
-    /**
-     * Returns a randomized QQueue of files to be used for playMusic()
-     * 
-     * @author Terence Stenvold
-     * @param dir The directory containing ogg files.
-     * @return A queue of filenames to be passed to playMusic().
-     */
-    QQueue<QString> musicDir(QString dir);
 
 };
 
