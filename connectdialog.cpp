89ffdf843d68dd3b3a561aefc6cc4a4102da05f5
Dean Morin <morin.dean@gmail.com>
2011-03-23 00:53:56 -0700

    pulled in all code from other branch for dialog

--- connectdialog.cpp
+++ connectdialog.cpp
@@ -1,7 +1,133 @@
 #include "connectdialog.h"
-#include <QtGui>
-
-ConnectDialog::ConnectDialog(QWidget* parent)
-{
-    //ui.setupUi(this);
-}
+#include <WinSock2.h>
+#include "commsocket.h"
+#include "defines.h"
+#include "ui_connect.h"
+//
+//ConnectDialog::ConnectDialog(QWidget *parent)
+//    : QDialog(parent), ui(new Ui::Connect()), ctlSock(NULL)
+//{
+//    ui->setupUi(this);
+//    
+//    connect(ui->connectPushButton, SIGNAL(clicked()),
+//            this, SLOT(onConnectClicked()));
+//    connect(ui->startServerPushButton, SIGNAL(clicked()),
+//            this, SLOT(onStartServerClicked()));
+//    connect(ui->multicastCheckBox, SIGNAL(stateChanged(int)),
+//            this, SLOT(onMulticastStateChanged(int)));
+//    
+//    multicastServer = ui->multicastCheckBox->isChecked();
+//}
+//
+//ConnectDialog::~ConnectDialog() {
+//    delete ui;
+//    delete ctlSock;
+//}
+//
+//void ConnectDialog::onConnectClicked() {
+//    unsigned long ip = 0;
+//    unsigned int port = 0;
+//    bool validPort = false;
+//    
+//    if ((ip = inet_addr(ui->ipLineEdit->text().toAscii())) == INADDR_NONE) {
+//        ui->connectErrorLabel->
+//                setText("The ip address must be in the form x.x.x.x");
+//        ui->ipLineEdit->selectAll();
+//        return;
+//    }
+//
+//    port = ui->serverPortLineEdit->text().toUInt(&validPort);
+//    if (!validPort || port < 1024 || port > 65535) {
+//        ui->connectErrorLabel->
+//                setText("The port number must be between 1024 and 65535");
+//        ui->serverPortLineEdit->selectAll();
+//        return;
+//    }
+//
+//    ui->connectErrorLabel->clear();
+//
+//    ui->connectPushButton->setDisabled(true);
+//    ui->startServerPushButton->setDisabled(true);
+//
+//    ctlSock = new CommSocket(this->ui->ipLineEdit->text(), port, 0);
+//    connect(ctlSock, SIGNAL(socketRead()), this, SLOT(onCtlReadReady()));
+//	connect(ctlSock, SIGNAL(socketWrite()), this, SLOT(onCtlWrite()));
+//
+//    if (!ctlSock->connectToServ()) {
+//        qDebug("Something went wrong trying to connect...");
+//    }
+//
+//    ui->connectPushButton->setDisabled(false);
+//    ui->connectPushButton->setText("Disconnect");
+//}
+//
+//void ConnectDialog::onStartServerClicked() {
+//    unsigned int port = 0;
+//    bool validPort = false;
+//
+//    // TODO: disable gui->components
+//
+//    port = ui->serverPortLineEdit->text().toUInt(&validPort);
+//    if (!validPort || port < 1024 || port > 65535) {
+//        ui->connectErrorLabel->
+//                setText("The port number must be between 1024 and 65535");
+//        ui->serverPortLineEdit->selectAll();
+//        return;
+//    }
+//
+//    disconnect(ui->startServerPushButton, SIGNAL(clicked()),
+//                this, SLOT(onStartServerClicked()));
+//
+//    ui->connectPushButton->setDisabled(true);
+//    ui->startServerPushButton->setText("Stop Server");
+//    connect(ui->startServerPushButton, SIGNAL(clicked()),
+//            this, SLOT(onStopServerClicked()));
+//
+//    if (ctlSock != NULL) {
+//        ctlSock->closeSocket();
+//        delete ctlSock;
+//    }
+//
+//    ctlSock = new CommSocket("", port, TCP);
+//    connect(ctlSock, SIGNAL(socketAccepted()), this, SLOT(onCtlAccept()));
+//    if (!ctlSock->listenForConn(5)) {
+//        qDebug("Something went wrong trying to listen...");
+//    }
+//}
+//
+//void ConnectDialog::onStopServerClicked() {
+//    disconnect(ui->startServerPushButton, SIGNAL(clicked()),
+//                this, SLOT(onStopServerClicked()));
+//
+//    ui->connectPushButton->setDisabled(false);
+//    ui->startServerPushButton->setText("Start Server");
+//    connect(ui->startServerPushButton, SIGNAL(clicked()),
+//            this, SLOT(onStartServerClicked()));
+//
+//    if (ctlSock == NULL) {
+//        return;
+//    }
+//
+//    ctlSock->closeSocket();
+//    delete ctlSock;
+//    ctlSock = NULL;
+//}
+//
+//void ConnectDialog::onMulticastStateChanged(int state) {
+//    multicastServer = ui->multicastCheckBox->isChecked();
+//}
+//
+//void ConnectDialog::onCtlReadReady() {
+//
+//    qDebug("Got something to read");
+//    QByteArray data = ctlSock->getReadBuffer();
+//    qDebug(data.data());
+//}
+//
+//void ConnectDialog::onCtlWrite() {
+//	qDebug("Got something to write");
+//}
+//
+//void ConnectDialog::onCtlAccept() {
+//    qDebug("Accepted a socket");
+//}
--- connectdialog.h
+++ connectdialog.h
@@ -1,17 +1,72 @@
 #ifndef CONNECTDIALOG_H
 #define CONNECTDIALOG_H
 
-#include <qdialog.h>
+#include <QDialog>
+class CommSocket;
+
+namespace Ui {
+    class Connect;
+}
 
 class ConnectDialog : public QDialog {
     Q_OBJECT
-
-private:
-    //Ui::ConnectDialogClass ui;
-
-public:
-    ConnectDialog(QWidget* parent = 0);
-
+//
+//private:
+//    /** The user interface for the dialog window. */
+//    Ui::Connect* ui;
+//    
+//    /** True if the program should multicast on clicking "Start Server". */
+//    bool multicastServer;
+//
+//    /** The open TCP socket either for the control channel. */
+//    CommSocket* ctlSock;
+//
+//    /** The open UDP socket for streaming data. */
+//    CommSocket* strSock;
+//
+//public:
+//    ConnectDialog(QWidget* parent = 0);
+//    ~ConnectDialog();
+//    
+//public slots:
+//    /**
+//     * Attempts to connect to the server. 
+//     *
+//     * @author Dean Morin
+//     */
+//    void onConnectClicked();
+//    
+//    /**
+//     * Starts listening for clients. 
+//     *
+//     * @author Dean Morin.
+//     */
+//    void onStartServerClicked();
+//
+//    /**
+//     * Stops the server socket from listening for incoming connections.
+//     *
+//     * @author Darryl Pogue
+//     */
+//    void onStopServerClicked();
+//
+//    /**
+//     * This server will multicast if this is selected.
+//     *
+//     * @author Dean Morin
+//     * @param state The state of multicastCheckBox.
+//     */
+//    void onMulticastStateChanged(int state);
+//
+//	void onCtlWrite();
+//    void onCtlAccept();
+//
+//    /**
+//     * Provides a notice that there is data available to be read on the control channel socket.
+//     *
+//     * @author Darryl Pogue
+//     */
+//    void onCtlReadReady();
 };
 
 #endif // CONNECTDIALOG_H
